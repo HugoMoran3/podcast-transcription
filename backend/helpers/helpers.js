af4f1635eaa0f00dc95f2432339c6b045ba8dd3b
HugoMoran3 <hugo.moran3@gmail.com>
2021-01-10 19:59:32 +1030

    Changed .gitignore

--- backend/helpers/helpers.js
+++ backend/helpers/helpers.js
@@ -1,24 +1,31 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const multer = require('multer')
+const util = require('util')
+const gc = require('./config/')
+const bucket = gc.bucket('podcast-bucket-react-node') // should be your bucket name
 
-const app = express()
+/**
+ *
+ * @param { File } object file object that will be uploaded
+ * @description - This function does the following
+ * - It uploads a file to the image bucket on Google Cloud
+ * - It accepts an object as an argument with the
+ *   "originalname" and "buffer" as keys
+ */
 
-const multerMid = multer({
-  storage: multer.memoryStorage(),
-  limits: {
-    fileSize: 5 * 1024 * 1024,
-  },
-})
+export const uploadImage = (file) => new Promise((resolve, reject) => {
+  const { originalname, buffer } = file
 
-app.disable('x-powered-by')
-app.use(multerMid.single('file'))
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: false}))
-
-app.post('/uploads', (req, res, next) => {
-})
-
-app.listen(9001, () => {
-  console.log('app now listening for requests!!!')
+  const blob = bucket.file(originalname.replace(/ /g, "_"))
+  const blobStream = blob.createWriteStream({
+    resumable: false
+  })
+  blobStream.on('finish', () => {
+    const publicUrl = format(
+      `https://storage.googleapis.com/${bucket.name}/${blob.name}`
+    )
+    resolve(publicUrl)
+  })
+  .on('error', () => {
+    reject(`Unable to upload image, something went wrong`)
+  })
+  .end(buffer)
 })
